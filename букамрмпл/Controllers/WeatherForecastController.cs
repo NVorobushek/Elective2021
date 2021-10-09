85cbe0c78eb129c60e6f006c52c93482baac0cbc
NVorobushek <nik290505@mail.ru>
2021-10-09 13:34:40 +0300

    vgjk

---
+++ букамрмпл/Controllers/WeatherForecastController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace букамрмпл.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost("{x}/{y}")]
+        public IActionResult Post([FromQuery] int x, [FromQuery] int y)
+        {
+            return Ok(x + y);
+        }
+    }
+}
